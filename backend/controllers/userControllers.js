c81691dbd0395cbff9c36199438b6560a0ba0215
Pratham Khanna <whilealivecode127.0.0.1@gmail.com>
2024-09-06 23:56:32 +0530

    Regular Development Update 5
    Developed user(chat) search API with middle ware authorization + chat API routes:
    1. Create new group
    2. Rename existing group
    3. Remove a user from existing group
    4. Add new user to existing group

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,19 +3,19 @@ const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
 
-// const allUsers = asyncHandler(async (req, res) => {
-//   const keyword = req.query.search
-//     ? {
-//         $or: [
-//           { name: { $regex: req.query.search, $options: "i" } },
-//           { email: { $regex: req.query.search, $options: "i" } },
-//         ],
-//       }
-//     : {};
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
 
-//   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
-//   res.send(users);
-// });
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
 
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -76,4 +76,4 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = {  registerUser, authUser };
+module.exports = {  registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const {
-  registerUser, authUser
+  registerUser, authUser, allUsers
 } = require("../controllers/userControllers");
 
-// const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // router.route("/").get(protect, allUsers);
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
